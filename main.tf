e9522e648c8412cf014976a8f5ed7e6553575eae
Ekene Umegakwe <95085212+kingsmandralph@users.noreply.github.com>
2022-10-17 19:32:13 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,3 +45,11 @@ resource "aws_instance" "myFirstInstance" {
   }
 }
 
+
+resource "aws_eip" "myFirstInstance" {
+  vpc      = true
+  instance = aws_instance.myFirstInstance.id
+tags= {
+    Name = "my_elastic_ip"
+  }
+}
