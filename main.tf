589d779cf5f16b3da60bb8f96cd3b13f6394e643
Ekene Umegakwe <95085212+kingsmandralph@users.noreply.github.com>
2022-10-17 20:10:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,39 +7,12 @@ terraform {
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
 
-#Create security group with firewall rules
-resource "aws_security_group" "my_security_group" {
-  name        = var.security_group
-  description = "security group for Ec2 instance"
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- # outbound from jenkis server
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags= {
-    Name = var.security_group
-  }
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
-
