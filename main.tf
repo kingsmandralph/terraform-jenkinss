6c028316461ad3f9801b638ea3cccb37ccf8e7d3
Ekene Umegakwe <95085212+kingsmandralph@users.noreply.github.com>
2022-10-17 19:47:08 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,22 +43,3 @@ resource "aws_security_group" "my_security_group" {
   }
 }
 
-# Create AWS ec2 instance
-resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id
-  key_name = var.key_name
-  instance_type = var.instance_type
-  security_groups= [var.security_group]
-  tags= {
-    Name = var.tag_name
-  }
-}
-
-
-resource "aws_eip" "myFirstInstance" {
-  vpc      = true
-  instance = aws_instance.myFirstInstance.id
-tags= {
-    Name = "my_elastic_ip"
-  }
-}
