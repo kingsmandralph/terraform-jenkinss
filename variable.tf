c2ebb44213ec74933a1c9804adcb39f4bc6b0214
Ekene Umegakwe <95085212+kingsmandralph@users.noreply.github.com>
2022-10-17 19:48:25 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,27 +3,8 @@ variable "aws_region" {
   default     = "eu-west-2"
 }
 
-variable "key_name" {
-  description = " SSH keys to connect to ec2 instance"
-  default     =  "Jenkins-keypair"
-}
-
-variable "instance_type" {
-  description = "instance type for ec2"
-  default     =  "t2.micro"
-}
-
 variable "security_group" {
   description = "Name of security group"
   default     = "my-jenkins-security-group"
 }
 
-variable "tag_name" {
-  description = "Tag Name of for Ec2 instance"
-  default     = "my-ec2-instance"
-}
-
-variable "ami_id" {
-  description = "AMI for Amazon linux instance"
-  default     = "ami-0464d49b8794eba32"
-}
