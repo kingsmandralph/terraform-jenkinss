ce1543c2e62a0eaf543ec0ecb846b84bad3bc0f4
Ekene Umegakwe <95085212+kingsmandralph@users.noreply.github.com>
2022-10-18 08:46:22 +0100

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,6 +1,14 @@
 variable "aws_region" {
-  description = "The AWS region to create things in."
+  description = "Default AWS Region"
   default     = "us-east-1"
 }
 
+variable "vpc_cidr1" {
+  description = "VPC CIDR by default"
+  default     = "10.20.0.0/16"
+}
 
+variable "vpc_cidr2" {
+  description = "VPC CIDR by default"
+  default     = "11.20.0.0/16"
+}
